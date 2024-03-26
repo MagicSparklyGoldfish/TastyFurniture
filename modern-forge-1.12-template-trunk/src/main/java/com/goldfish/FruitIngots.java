fc1d4415ca741b40e2d8d5089b383f0fe2215bc2
MagicSparklyGoldfish <whydeargodwhy@yahoo.com~>
2024-03-26 13:12:59 -0400

    made class

---
+++ modern-forge-1.12-template-trunk/src/main/java/com/goldfish/FruitIngots.java
@@ -0,0 +1,13 @@
+package com.goldfish;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.FurnaceRecipes;
+
+public class FruitIngots extends Item {
+       public FruitIngots() {
+        setRegistryName("Apple Ingot");  
+        //setUnlocalizedName(FruitIngots.MODID + ".Apple Ingot"); 
+       } //appleIngot = new FruitIngots();
+    
+}
