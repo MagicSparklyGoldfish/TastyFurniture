3edcea292da43e7f3133008d59781e10a2228d7a
MagicSparklyGoldfish <whydeargodwhy@yahoo.com~>
2024-03-26 12:27:48 -0400

    created mod id

--- modern-forge-1.12-template-trunk/src/main/java/com/goldfish/TastyFurniture.java
+++ modern-forge-1.12-template-trunk/src/main/java/com/goldfish/TastyFurniture.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 	version = TastyFurniture.VERSION
 )
 public class TastyFurniture {
-	public static final String MODID = "samplemod112";
+	public static final String MODID = "goldfishfurnituremod01";
 	public static final String NAME = "Tasty Furniture";
 	public static final String VERSION = "0.1";
 	
@@ -22,7 +22,3 @@ public class TastyFurniture {
 		LOGGER.info("Hello, world!");
 	}
 }
-
-//changing the MODID causes minecraft to not recognize this as a mod
-//changing the name is fine
-//changing the version is also fine
