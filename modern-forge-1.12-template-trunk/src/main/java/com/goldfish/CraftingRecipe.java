8e02d202eb301ec6653d80378e8cf2f4a09b8d9b
MagicSparklyGoldfish <whydeargodwhy@yahoo.com~>
2024-03-29 08:28:28 -0400

    added crafting recipe class.

---
+++ modern-forge-1.12-template-trunk/src/main/java/com/goldfish/CraftingRecipe.java
@@ -0,0 +1,12 @@
+package com.goldfish;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import com.goldfish.TastyFurniture;
+import com.goldfish.FruitIngots;
+
+public class CraftingRecipe {
+    
+    
+}
--- modern-forge-1.12-template-trunk/src/main/java/com/goldfish/FruitIngots.java
+++ modern-forge-1.12-template-trunk/src/main/java/com/goldfish/FruitIngots.java
@@ -3,9 +3,11 @@ package com.goldfish;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.crafting.FurnaceRecipes;
+import com.goldfish.TastyFurniture;
 
 public class FruitIngots extends Item {
        public FruitIngots() {
+       //name
         setRegistryName("Apple Ingot");  
         //setUnlocalizedName(FruitIngots.MODID + ".Apple Ingot"); 
        } //appleIngot = new FruitIngots();
